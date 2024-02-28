58cded462c191f76eab06a6df56239aec17fbf8c
lucasseccatto <lucasseccatto1@gmail.com>
2024-02-28 14:10:44 -0300

    Refatorado removendo a barra final na URL do baseURL:

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "18c0388af8a48131d618dbf1f971758c",
     language: "pt-BR",
