313cb22a7925c3685f6d4a1de0505b947011ec84
lucasseccatto <lucasseccatto1@gmail.com>
2024-02-28 14:08:18 -0300

    Criacao do component Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 import Logo from "../../assets/logo.png";
-import { StyledHeader, StyledLogo } from "./styles";
+import { Container, Menu, Li } from "./styles";
 
 function Header() {
+  const [changeBackground, setChangeBackground] = useState(false);
+  const { pathname } = useLocation();
+
+  window.onscroll = () => {
+    if (!changeBackground && window.pageYOffset > 150) {
+      setChangeBackground(true);
+    }
+    if (changeBackground && window.pageYOffset <= 150) {
+      setChangeBackground(false);
+    }
+  };
+
   return (
-    <StyledHeader>
-      <StyledLogo src={Logo} alt="logo-dev-movies" />
-    </StyledHeader>
+    <Container changeBackground={changeBackground}>
+      <img src={Logo} alt="logo-dev-movies" />
+      <Menu>
+        <Li isActive={pathname === "/"}>
+          <Link to="/">Home</Link>
+        </Li>
+        <Li isActive={pathname.includes("filmes")}>
+          <Link to="/filmes">Filmes</Link>
+        </Li>
+        <Li isActive={pathname.includes("series")}>
+          <Link to="/series">Séries</Link>
+        </Li>
+      </Menu>
+    </Container>
   );
 }
 
