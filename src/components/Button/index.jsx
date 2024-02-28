f42e57eb2cbc104b08c314d195a59238d3133eb1
lucasseccatto <lucasseccatto1@gmail.com>
2024-02-28 14:07:54 -0300

    Criacao do component Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,14 @@
-function Button() {
-  return <button>Abacate</button>;
-}
+import { ButtonRed, ButtonWhite } from "./styles";
 
+function Button({ children, red, ...rest }) {
+  return (
+    <>
+      {red ? (
+        <ButtonRed {...rest}>{children}</ButtonRed>
+      ) : (
+        <ButtonWhite {...rest}>{children}</ButtonWhite>
+      )}
+    </>
+  );
+}
 export default Button;
